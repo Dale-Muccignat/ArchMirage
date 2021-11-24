09f2be241c07d9a88e0839099e23ba973ec866bc
Dale-Muccignat <dale.muccignat@my.jcu.edu.au>
2021-11-24 17:19:32 +1100

    Place chroot commands into a seperate file

--- installcommands.sh
+++ installcommands.sh
@@ -25,58 +25,65 @@ pacstrap /mnt base linux linux-firmware networkmanager vim alacritty git base-de
 # Notice how it'll detect where to install through the mount point /mnt. I also installed networkmanager and vim for internet and text editing. 
 # Now we write down the partition table of fstab for linux to read
 genfstab -U /mnt >> /mnt/etc/fstab
+
+#First we generate a file on the mounted filesystem to run from a chroot environment, taken from https://bbs.archlinux.org/viewtopic.php?id=204252
+cat <<EOF > /mnt/root/part2.sh
+ # Set the timezone
+ ln -sf /usr/share/zoneinfo/Australia/Melbourne /etc/localtime
+ # Run hwclock to make sure local/system time and good
+ hwclock --systohc
+ # Now the fun edit file part. I use vim here but maybe use a different text editor haha
+ # Edit /etc/local.gen and uncomment en_US.UTF-8 UTF-8
+ #vim /etc/locale.gen
+ # Use j/k to go up down until you find the line. Move curse to the # on that line and hit "x" to delete it. Type ":wq" to save and quit.
+ # Generate the locales (idek what this is doing but it's some weird filesystem thing)
+ locale-gen
+ # Edit /etc/locale.conf
+ #vim /etc/locale.conf
+ # hit "i" to enter edit mode and type "LANG=en_US.UTF-8". hit "esc" to exit edit mode and then type ":wq" to save and quit.
+ # Make a root password
+ #passwd
+ # Time for bootloaded. I got this from the GRUB page of archwiki. Install grub and efibootmgr:
+ pacman -S grub efibootmgr
+ # then install grub to a mount point:
+ mkdir /mnt/boot
+ mount /dev/sda1 /mnt/boot
+ grub-install --target=x86_64-efi --efi-directory=/mnt/boot --bootloader-id=GRUB
+ # then generate the GRUB config file
+ grub-mkconfig -o /boot/grub/grub.cfg
+
+ # At this point you can restart and log into root just fine but lets set up our display manager and desktop environment.
+ # So display manager handles the log in and handles the overal "display" and the you log into a desktop envionment from the display manager.
+ # There's a few display managers to choose from but I'm going to use GDM - IDK if it's the best choice tbh but who cares.
+ pacman -S gdm cinnamon
+ # Just select the default option.
+ # Now we need to enable gdm so that it starts on startup through
+ systemctl enable gdm.service
+ # Might aswell enable networkmanager here aswell
+ systemctl enable NetworkManager.service
+ # Then just add a user:
+ useradd -m dale
+ #passwd dale
+ # And just need to set up the "sudo" shit
+ pacman -S sudo
+ # then edit /etc/sudoers and add "dale ALL=(ALL) ALL" under the user alias specification section
+ #visudo /etc/sudoers
+ # use j/k to move to under the user alias section. Hit "i" to enter edit mode. Type out the line then hit "esc" to exit edit mode. Then type ":wq" to write and quit.
+ # Now we can ctrl+d to exit chroot and then reboot and remove the usb. 
+ # SAVE THE USB, you can use it to save your computer if it dies. You can chroot into 
+ # PRO TIPS: 
+ # Edit /etc/pacman.conf to enable parralel downloads (I'd say like 6)
+ # install "tldr" so that you can go "tldr zip" or some shit when you don't know how to use a command/program.
+
+
+ # Install yay
+ #git clone https://aur.archlinux.org/yay.git
+ #cd yay
+ #makepkg -si --noconfirm
+ #cd ..
+
+exit # to leave the chroot
+EOF
+
 # And now we can chroot into linux!
-arch-chroot /mnt
-# Set the timezone
-ln -sf /usr/share/zoneinfo/Australia/Melbourne /etc/localtime
-# Run hwclock to make sure local/system time and good
-hwclock --systohc
-# Now the fun edit file part. I use vim here but maybe use a different text editor haha
-# Edit /etc/local.gen and uncomment en_US.UTF-8 UTF-8
-vim /etc/locale.gen
-# Use j/k to go up down until you find the line. Move curse to the # on that line and hit "x" to delete it. Type ":wq" to save and quit.
-# Generate the locales (idek what this is doing but it's some weird filesystem thing)
-locale-gen
-# Edit /etc/locale.conf
-vim /etc/locale.conf
-# hit "i" to enter edit mode and type "LANG=en_US.UTF-8". hit "esc" to exit edit mode and then type ":wq" to save and quit.
-# Make a root password
-passwd
-# Time for bootloaded. I got this from the GRUB page of archwiki. Install grub and efibootmgr:
-pacman -S grub efibootmgr
-# then install grub to a mount point:
-mkdir /mnt/boot
-mount /dev/sda1 /mnt/boot
-grub-install --target=x86_64-efi --efi-directory=/mnt/boot --bootloader-id=GRUB
-# then generate the GRUB config file
-grub-mkconfig -o /boot/grub/grub.cfg
-
-# At this point you can restart and log into root just fine but lets set up our display manager and desktop environment.
-# So display manager handles the log in and handles the overal "display" and the you log into a desktop envionment from the display manager.
-# There's a few display managers to choose from but I'm going to use GDM - IDK if it's the best choice tbh but who cares.
-pacman -S gdm cinnamon
-# Just select the default option.
-# Now we need to enable gdm so that it starts on startup through
-systemctl enable gdm.service
-# Might aswell enable networkmanager here aswell
-systemctl enable NetworkManager.service
-# Then just add a user:
-useradd -m dale
-passwd dale
-# And just need to set up the "sudo" shit
-pacman -S sudo
-# then edit /etc/sudoers and add "dale ALL=(ALL) ALL" under the user alias specification section
-visudo /etc/sudoers
-# use j/k to move to under the user alias section. Hit "i" to enter edit mode. Type out the line then hit "esc" to exit edit mode. Then type ":wq" to write and quit.
-# Now we can ctrl+d to exit chroot and then reboot and remove the usb. 
-# SAVE THE USB, you can use it to save your computer if it dies. You can chroot into 
-# PRO TIPS: 
-# Edit /etc/pacman.conf to enable parralel downloads (I'd say like 6)
-# install "tldr" so that you can go "tldr zip" or some shit when you don't know how to use a command/program.
-
-
-# Install yay
-git clone https://aur.archlinux.org/yay.git
-cd yay
-makepkg -si --noconfirm
-cd ..
+arch-chroot /mnt /root/part2.sh
