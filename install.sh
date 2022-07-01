8a56368e3b879452d79b7556f4871d17d7a75070
Dale Muccignat <dale.muccignat@my.jcu.edu.au>
2022-07-01 10:02:15 +1000

    update

--- install.sh
+++ install.sh
@@ -34,6 +34,8 @@ genfstab -U /mnt >> /mnt/etc/fstab
 
 cp -R ../ArchMirage /mnt
 
+echo "drive=$drive" >> /mnt/ArchMirage/install.conf
+
 # And now we can chroot into linux!
 arch-chroot /mnt /bin/bash ArchMirage/chroot.sh
 source /mnt/root/ArchMirage/install.conf
