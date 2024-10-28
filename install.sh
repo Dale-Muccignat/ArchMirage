ede381e8c949266ce736badad59404a0ed76e4bb
Dale Muccignat <dale.muccignat@my.jcu.edu.au>
2024-10-28 23:52:56 +0000

    Add initial DM selection

--- install.sh
+++ install.sh
@@ -1,4 +1,38 @@
 #!/bin/bash
+function choose_from_menu() {
+  local prompt="$1" outvar="$2"
+  shift
+  shift
+  local options=("$@") cur=0 count=${#options[@]} index=0
+  local esc=$(echo -en "\e") # cache ESC as test doesn't allow esc codes
+  printf "$prompt\n"
+  while true; do
+    # list all options (option list is zero-based)
+    index=0
+    for o in "${options[@]}"; do
+      if [ "$index" == "$cur" ]; then
+        echo -e " >\e[7m$o\e[0m" # mark & highlight the current option
+      else
+        echo "  $o"
+      fi
+      ((index++))
+    done
+    read -s -n3 key               # wait for user to key in arrows or ENTER
+    if [[ $key == $esc[A ]]; then # up arrow
+      ((cur--))
+      ((cur < 0)) && ((cur = 0))
+    elif [[ $key == $esc[B ]]; then # down arrow
+      ((cur++))
+      ((cur >= count)) && ((cur = count - 1))
+    elif [[ $key == "" ]]; then # nothing, i.e the read delimiter - ENTER
+      break
+    fi
+    echo -en "\e[${count}A" # go up to the beginning to re-render
+  done
+  # export the selection to the requested output variable
+  printf -v $outvar "${options[$cur]}"
+}
+
 # Update keyring, don't know why this is needed now
 pacman -Sy --noconfirm archlinux-keyring
 # Update system clock:
@@ -15,6 +49,15 @@ sfdisk "/dev/${drive}" < sda.sfdisk
  # 1. 1G efi filesystem: This is where the bootloader will mount to.
  # 2. 8G swarp partition: This is optional but could help with ram or something like that
  # 3. Linux filesystem: Rest of storage. this is where we will mount linux
+
+# Ask for DMs to install
+# taken from Guss at https://askubuntu.com/questions/1705/how-can-i-create-a-select-menu-in-a-shell-script
+
+
+DMs=("Plasma" "Gnome" "i3")
+choose_from_menu "Choose a display manager to install:" selected_DM "${DMs[@]}"
+echo "Selected display manager: $selected_DM"
+
 
 # Define partition names properly
 # sd/vd are named with just the number
@@ -59,6 +102,7 @@ cp -R ../ArchMirage /mnt
 
 # save drive info
 echo "drive=$drive" >> /mnt/ArchMirage/install.conf
+echo "selected_DM=$selected_DM" >> /mnt/ArchMirage/install.conf
 
 # And now we can chroot into linux!
 arch-chroot /mnt /bin/bash ArchMirage/chroot.sh
