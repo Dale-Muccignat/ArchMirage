88361ccee247a8548ce3bf26dbc66598636318a4
Dale Muccignat <dale.muccignat@my.jcu.edu.au>
2024-10-29 00:51:41 +0000

    revert shift

--- install.sh
+++ install.sh
@@ -3,7 +3,6 @@ function choose_from_menu() {
   local prompt="$1" outvar="$2"
   shift
   shift
-  shift
   local options=("$@") cur=0 count=${#options[@]} index=0
   local esc=$(echo -en "\e") # cache ESC as test doesn't allow esc codes
   printf "$prompt\n"
